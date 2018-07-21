efc73bf69dc88bf96d795e12af73b74c852ae69f
Song <hailusong@hotmail.com>
2018-07-21 05:51:46 -0400

    trail-and-error; add comments

---
+++ examples/twenty_newsgroups/data/copy-data-over.sh
@@ -0,0 +1,2 @@
+scp aimladmin@10.115.3.165:/home/aimladmin/notebooks/home/ksong/lda2vec/examples/twenty_newsgroups/data/*.npy .
+scp aimladmin@10.115.3.165:/home/aimladmin/notebooks/home/ksong/lda2vec/examples/twenty_newsgroups/data/*.pkl .
---
+++ examples/twenty_newsgroups/lda2vec/touch-googlenewsvec.sh
@@ -0,0 +1,1 @@
+ln -s ~/notebooks/home/ksong/GoogleNews-vectors-negative300.bin GoogleNews-vectors-negative300.bin
